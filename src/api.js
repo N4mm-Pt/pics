462055ac2b290a23024d07510189eac3ceca8f06
RyanPham <ryan.pham@grapecity.com>
2023-08-11 15:57:57 +0700

    update

--- src/api.js
+++ src/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos',{
     headers: {
       Authorization: 'Client-ID a70XZ9uD7xBoDN1xMiNOe72i71H8uKLM8zFNsiCmAag'
     },
     params: {
-      query: 'cars'
+      query: term,
     }
   });
 
-  return response;
+  return response.data.results;
 }
 
 export default searchImages;
